e23ba9346f6ad640c94bbad35eb70b2d942c5c82
Henrique Lobato Zago <lobato.zago@gmail.com>
2017-05-21 17:52:14 -0300

    Added another user on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create({
+User.create!([{
   name: 'Henrique',
   email: 'henrique@example.com',
   password: '12345'
-})
+}, {
+  name: 'Joares',
+  email: 'henrique@example.com',
+  password: '12345'
+}])
 
-Employee.create!([
-  { name: 'Henrique Zago', role: 0, birth_date: Date.now },
-  { name: 'Heloisy Zago', role: 1, birth_date: Date.now }
+Employee.create([
+  { name: 'Henrique Zago', role: 0, birth_date: Date.new },
+  { name: 'Heloisy Zago', role: 1, birth_date: Date.new }
 ])
